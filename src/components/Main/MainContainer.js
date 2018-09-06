7cc2f56f10bd7dea4cd5e55bd743024ec016d2af
Kael <piskael03@gmail.com>
2018-09-06 13:16:38 -0400

    config react router

--- src/components/Main/MainContainer.js
+++ src/components/Main/MainContainer.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
-import {Provider as AppDataProvider} from '../../appContext';
+import {Provider as AppDataProvider} from 'appContext';
 import {theme} from 'theme/globalStyle';
 import store from 'store';
-import {LandingPage} from "../Pages/";
 
-
+import {HashRouter, Route, Switch} from 'react-router-dom'
+import {LandingPage} from "../Pages/LandingPage";
+import {AboutPage} from "../Pages/About";
 const MainContainer = () => (
     <AppDataProvider value={store}>
         <ThemeProvider theme={theme}>
-            <LandingPage/>
+            <HashRouter>
+                <Switch>
+                    <Route exact path='/' component={LandingPage}/>
+                    <Route exact path='/about' component={AboutPage}/>
+                </Switch>
+            </HashRouter>
         </ThemeProvider>
     </AppDataProvider>
 );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainContainer from './components/Main/MainContainer';
+import MainContainer from "./components/Main/MainContainer";
 
 ReactDOM.render(<MainContainer/>, document.getElementById('root'));
